0945889d25db1e5dca32c84dc63f717c37767d88
Brendan Sheehan <feemcgill@gmail.com>
2019-02-13 13:37:48 -0800

    adding stereolab

--- src/audio/playlist.js
+++ src/audio/playlist.js
@@ -106,7 +106,101 @@ const ddTracks = [
   {
     'text' : 'White Fence - To the Boy I Jumped in the Hemlock Alley',
     'url' : 'White+Fence+-+To+the+Boy+I+Jumped+in+the+Hemlock+Alley.mp3'
-  }
+  },
+  /* STEREOLAB STUFF */
+  {
+    'text' : 'Stereolab - Brakhage',
+    'url' : 'sl/Dots+and+Loops/01.+Brakhage.mp3'
+  },
+  {
+    'text' : 'Stereolab - Miss Modular',
+    'url' : 'sl/Dots+and+Loops/02.+Miss+Modular.mp3'
+  },
+  {
+    'text' : 'Stereolab - The Flower Called Nowhere',
+    'url' : 'sl/Dots+and+Loops/03.+The+Flower+Called+Nowhere.mp3'
+  },
+  {
+    'text' : 'Stereolab - Diagonals',
+    'url' : 'sl/Dots+and+Loops/04.+Diagonals.mp3'
+  },
+  {
+    'text' : 'Stereolab - Prisoner of Mars',
+    'url' : 'sl/Dots+and+Loops/05.+Prisoner+of+Mars.mp3'
+  },
+  {
+    'text' : 'Stereolab - Rainbo Conversation',
+    'url' : 'sl/Dots+and+Loops/06.+Rainbo+Conversation.mp3'
+  },
+  {
+    'text' : 'Stereolab - Refractions in the Plastic Pulse',
+    'url' : 'sl/Dots+and+Loops/07.+Refractions+in+the+Plastic+Pulse.mp3'
+  },
+  {
+    'text' : 'Stereolab - Parsec',
+    'url' : 'sl/Dots+and+Loops/08.+Parsec.mp3'
+  },
+  {
+    'text' : 'Stereolab - Ticker-Tape of the Unconscious',
+    'url' : 'sl/Dots+and+Loops/09.+Ticker-Tape+of+the+Unconscious.mp3'
+  },
+  {
+    'text' : 'Stereolab - Contronatura',
+    'url' : 'sl/Dots+and+Loops/10.+Contronatura.mp3'
+  },
+  {
+    'text' : 'Stereolab - Metronomic Underground',
+    'url' : 'sl/Emperor+Tomato+Ketchup/01.+Metronomic+Underground.mp3'
+  },
+  {
+    'text' : 'Stereolab - Cybele_s Reverie',
+    'url' : 'sl/Emperor+Tomato+Ketchup/02.+Cybele_s+Reverie.mp3'
+  },
+  {
+    'text' : 'Stereolab - Percolator',
+    'url' : 'sl/Emperor+Tomato+Ketchup/03.+Percolator.mp3'
+  },
+  {
+    'text' : 'Stereolab - Les Yper-Sound',
+    'url' : 'sl/Emperor+Tomato+Ketchup/04.+Les+Yper-Sound.mp3'
+  },
+  {
+    'text' : 'Stereolab - Spark Plug',
+    'url' : 'sl/Emperor+Tomato+Ketchup/05.+Spark+Plug.mp3'
+  },
+  {
+    'text' : 'Stereolab - Olv 26',
+    'url' : 'sl/Emperor+Tomato+Ketchup/06.+Olv+26.mp3'
+  },
+  {
+    'text' : 'Stereolab - The Noise Of Carpet',
+    'url' : 'sl/Emperor+Tomato+Ketchup/07.+The+Noise+Of+Carpet.mp3'
+  },
+  {
+    'text' : 'Stereolab - Tomorrow Is Already Here',
+    'url' : 'sl/Emperor+Tomato+Ketchup/08.+Tomorrow+Is+Already+Here.mp3'
+  },
+  {
+    'text' : 'Stereolab - Emperor Tomato Ketchup',
+    'url' : 'sl/Emperor+Tomato+Ketchup/09.+Emperor+Tomato+Ketchup.mp3'
+  },
+  {
+    'text' : 'Stereolab - Monstre Sacre',
+    'url' : 'sl/Emperor+Tomato+Ketchup/10.+Monstre+Sacre.mp3'
+  },
+  {
+    'text' : 'Stereolab - Motoroller Scalatron',
+    'url' : 'sl/Emperor+Tomato+Ketchup/11.+Motoroller+Scalatron.mp3'
+  },
+  {
+    'text' : 'Stereolab - Slow Fast Hazel',
+    'url' : 'sl/Emperor+Tomato+Ketchup/12.+Slow+Fast+Hazel.mp3'
+  },
+  {
+    'text' : 'Stereolab - Anonymous Collective',
+    'url' : 'sl/Emperor+Tomato+Ketchup/13.+Anonymous+Collective.mp3'
+  },
+
 ]
 
 export default ddTracks;
